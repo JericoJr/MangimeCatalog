fd8aa83e2733eb76a074cdfb0b9f5b133da405f3
JericoJr <ricothestar05@gmail.com>
2025-12-10 22:26:58 -0500

    finished myStuff and Browse page

--- routes/addContent.js
+++ routes/addContent.js
@@ -1,75 +1,84 @@
 const express = require('express');
 const router = express.Router();
-
 const mongoose = require("mongoose");
 
-router.get("/", (request, response) => {
-    console.log("\nEntering Form Page");
-    response.render("addContentForm", {message: null});
+//Schema for Anime, Manga
+const animeSchema = new mongoose.Schema({
+  title: String,
+  type: String,
+  status: String,
+  genre: [String],
+  rating: Number,
+  comments: String
 });
 
+const mangaSchema = new mongoose.Schema({
+  title: String,
+  type: String,
+  status: String,
+  genre: [String],
+  rating: Number,
+  comments: String
+});
+
+//Creates model only once, if exists using exisiting, otherwise create new one
+const Anime = mongoose.models.Anime || mongoose.model("Anime", animeSchema);
+const Manga = mongoose.models.Manga || mongoose.model("Manga", mangaSchema);
+
+router.get("/", (request, response) => {
+  console.log("\nEntering Form Page");
+  let displayMessage;
+  if (request.query.message) {
+    displayMessage = JSON.parse(request.query.message);
+  } else {
+    displayMessage = null;
+  }
+  response.render("addContentForm", {message: displayMessage});
+});
 
 router.post("/submit", async (request, response) => {  
-    console.log('POST request to /addContentForm/submit');
-    let resultMessage = await addToDB(request);
-    console.log(resultMessage)
-    response.render("addContentForm", {message: resultMessage});
+  console.log('POST request to /addContentForm/submit');
+  let result = await addToDB(request);
+  let message = ""; 
+  if (result) {
+    message += `<p class="succ-message">Item was Successfully Addded!</p>`;
+  } else {
+    message += `<p class="err-message">Error: Item was Not Successfully Added</p>`;
+  }
+  const messageEnc = encodeURIComponent(JSON.stringify(message));
+  response.redirect(`/addContent?message=${messageEnc}`);
 });
 
-
 async function addToDB(request) {
-    try {
-      await mongoose.connect(process.env.MONGO_CONNECTION_STRING, { dbName: "contentDB"});
-    
-        if (request.body.type === "Anime") {
-        const animeSchema = new mongoose.Schema({
-            title: String,
-            type: String,
-            status: String,
-            genre: [String],
-            rating: Number,
-            comments: String
-        });
-
-        const Anime = mongoose.model("Anime", animeSchema);
-
-        await Anime.create({
-            title: request.body.title,
-            type: "Anime",
-            status: request.body.status,
-            genre: request.body.genre,
-            rating: request.body.rating,
-            comments: request.body.comments
-        });
-        console.log("\nAdded Anime to Database");
-      } else {
-            const mangaSchema = new mongoose.Schema({
-            title: String,
-            type: String,
-            status: String,
-            genre: [String],
-            rating: Number,
-            comments: String
-        });
-
-        const Manga = mongoose.model("Manga", mangaSchema);
-
-        await Manga.create({
-            title: request.body.title,
-            type: "Manga",
-            status: request.body.status,
-            genre: request.body.genre,
-            rating: request.body.rating,
-            comments: request.body.comments
-        }); 
-        console.log("\nAdded Manga to Database");
-      }
-        mongoose.disconnect();
-    } catch (err) {
-      console.error(err);
-      return `<p class="err-message">Error: ${request.body.title} was Not Added to Database</p>`;
+  try {
+    await mongoose.connect(process.env.MONGO_CONNECTION_STRING, { dbName: "contentDB"});
+    if (request.body.type === "Anime") {
+      await Anime.create({
+        title: request.body.title,
+        type: "Anime",
+        status: request.body.status,
+        genre: request.body.genre,
+        rating: request.body.rating,
+        comments: request.body.comments
+      });
+      console.log("\nAdded Anime to Database");
+    } else {
+      await Manga.create({
+        title: request.body.title,
+        type: "Manga",
+        status: request.body.status,
+        genre: request.body.genre,
+        rating: request.body.rating,
+        comments: request.body.comments
+      }); 
+      console.log("\nAdded Manga to Database");
     }
-    return `<p class="succ-message">"${request.body.title}" was Sucessfully Added to Database!</p>`;
+      mongoose.disconnect();
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+  return true;
 }
 
 module.exports = router; 
--- routes/myStuff.js
+++ routes/myStuff.js
@@ -4,10 +4,55 @@ const router = express.Router();
 const mongoose = require("mongoose");
 
 router.get("/", async (request, response) => {
-    console.log("\nEntering myStuff Page");
-    animeTable = await getTable("Anime");
-    mangaTable = await getTable("Manga");
+    console.log("Entering myStuff Page");
+    let animeTable;
+    let mangaTable;
+    //Checks for already exisiting table parameters
+    if (request.query.animeTable && request.query.mangaTable) {
+        animeTable = JSON.parse(request.query.animeTable);
+        mangaTable = JSON.parse(request.query.mangaTable);
+    } else {
+        //Note: earliest is default sort order
+        animeTable = await getTable("Anime", "earliest");
+        mangaTable = await getTable("Manga", "earliest");
+    }
+   
     response.render("myStuff", {animeTable: animeTable, mangaTable: mangaTable});
+});
+
+router.post("/filter", async (request, response) => {
+    const type = request.body.filterType;
+    const sortFilter = request.body.sortFilter;
+    let animeTable;
+    let mangaTable;
+
+    if (type === "Both") {
+        animeTable = await getTable("Anime", sortFilter);
+        mangaTable = await getTable("Manga", sortFilter);
+    } else {
+        if (type === "Anime") {
+            animeTable = await getTable("Anime", sortFilter);
+            mangaTable = await getTable("Manga", "earliest");
+        } else {
+            animeTable = await getTable("Anime", "earliest");
+            mangaTable = await getTable("Manga", sortFilter);
+        }
+    }
+    //convert tables into string for url
+    const animeTableEnc = encodeURIComponent(JSON.stringify(animeTable));
+    const mangaTableEnc = encodeURIComponent(JSON.stringify(mangaTable));
+    response.redirect(`/myStuff?animeTable=${animeTableEnc}&mangaTable=${mangaTableEnc}`);
+});
+
+router.post("/delContent", async (request, response) => {
+    let ID = request.body.contentID;
+    let type = request.body.contentType;
+    if (type === "Anime") {
+        await delAnimeContent(ID);
+    } else {
+        await delMangaContent(ID);
+    }
+    response.redirect("/myStuff");
 });
 
 router.post("/deleteAll", async (request, response) => {
@@ -25,7 +70,7 @@ router.post("/deleteAll", async (request, response) => {
     response.redirect("/myStuff");
 });
 
-async function getTable(type) {
+async function getTable(type, sortFilter) {
     let idName;
     if (type === "Anime") {
         idName = "animeTable";
@@ -34,36 +79,85 @@ async function getTable(type) {
     }
     let table = `<table class="myStuffTable" id="${idName}">`;
     table += `<colgroup>
+                <col style="width: 2%">   
                 <col style="width: 25%">   
                 <col style="width: 4%">  
                 <col style="width: 20%">   
-                <col style="width: 10%">  
+                <col style="width: 8%">  
                 <col style="width: 1%">
                 <col style="width: 25%"> 
                 <col style="width: 25%"> 
             </colgroup>`;
-    table += `<thead> <tr> <th>Title</th> <th>Type</th> <th>Genre</th> <th>Status</th> <th>Rating</th> <th>Comments</th> <th></th></tr> <thead>`;
+    table += `<thead> <tr> <th>#</th> <th>Title</th> <th>Type</th> <th>Genre</th> <th>Status</th> <th>Rating</th> <th>Comments</th> <th></th></tr> <thead>`;
     try {
         await mongoose.connect(process.env.MONGO_CONNECTION_STRING, { dbName: "contentDB"});
         let collectionName = (type === "Anime" ? "animes":"mangas");
         const animeCollection = mongoose.connection.db.collection(collectionName);
-        const animeData = await animeCollection.find({}).toArray();
+        let animeData; 
+        //Sorts Database first according to sortFilter
+        if (sortFilter !== "earliest") {
+            if (sortFilter === "newest") {
+                animeData = await animeCollection.find({}).sort({ _id: -1}).toArray();
+            } else if (sortFilter === "title-asc") {
+                animeData = await animeCollection.find({}).sort({ title: 1}).toArray();
+            } else if (sortFilter === "title-des") {
+                animeData = await animeCollection.find({}).sort({ title: -1}).toArray();
+            } else if (sortFilter === "status") {
+                animeData = await animeCollection.find({}).sort({ status: 1}).toArray();
+            } else if (sortFilter === "rating-asc") {
+                animeData = await animeCollection.find({}).sort({ rating: 1}).toArray();
+            } else if (sortFilter === "rating-des") {
+                animeData = await animeCollection.find({}).sort({ rating: -1}).toArray();
+            } 
+        } else {
+            animeData = await animeCollection.find({}).toArray();
+        }
+        
+        //Get data from database and formats each data and its field as a table row
         let tableEntries = "";
+        let count = 0;
         animeData.forEach( content => {
-            tableEntries += `<tr> <td>${content.title}</td> <td>${content.type}</td> <td>${content.genre}</td> <td>${content.status}</td> <td>${content.rating}</td> <td>${content.comments}</td> <th> <button>Edit</button> <button>Delete</button> </th> </tr>`;
+            tableEntries += `<tr> <td>${++count}</td> <td>${content.title}</td> <td>${content.type}</td> <td>${content.genre}</td> <td>${content.status}</td> <td>${content.rating}</td> <td>${content.comments}</td> <th> <form action="myStuff/delContent" method="POST"> <input type="hidden" name="contentID" value="${content._id}"> <input type="hidden" name="contentType" value="${type}"> <button type="submit">Delete</button> </form> </th> </tr>`;
         });
         if (tableEntries === "") {
-            table += `<tbody class="myStuffTableBody"> <tr> <td colspan="7">None</td> </tr> </tbody>`;
+            table += `<tbody class="myStuffTableBody"> <tr> <td colspan="8">None</td> </tr> </tbody>`;
         } else {
             table += `<tbody class="myStuffTableBody"> ${tableEntries} </tbody>`;
         }
         mongoose.disconnect();
     } catch (err) {
       console.error(err);
-      table += `<tbody class="myStuffTableBody"> <tr> <td colspan="7">None</td> </tr> </tbody>`;
+      table += `<tbody class="myStuffTableBody"> <tr> <td colspan="8">None</td> </tr> </tbody>`;
     }
     table += `</table>`;
     return table;
 }
 
+async function delAnimeContent(id) {
+    const objectID = new mongoose.Types.ObjectId(id);
+    try {
+        console.log(`deleting Anime - ${id}`);
+        await mongoose.connect(process.env.MONGO_CONNECTION_STRING, { dbName: "contentDB"});
+        const animeCollection = mongoose.connection.db.collection("animes");
+        await animeCollection.deleteOne({ _id: objectID });
+        mongoose.disconnect();
+    } catch (err) {
+      console.error(err);
+    }
+    return;
+}
+
+async function delMangaContent(id) {
+     try {
+        console.log(`deleting Manga - ${id}`);
+        const objectID =  new mongoose.Types.ObjectId(id);
+        await mongoose.connect(process.env.MONGO_CONNECTION_STRING, { dbName: "contentDB"});
+        const mangaCollection = mongoose.connection.db.collection("mangas");
+        await mangaCollection.deleteOne({ _id: objectID });
+        mongoose.disconnect();
+    } catch (err) {
+      console.error(err);
+    }
+    return;
+}
 module.exports = router; 
