e2306d7538a0f845207715464a981a57668a73d3
JericoJr <jericojrestrella@gmail.com>
2026-01-07 22:58:53 -0500

    style login and signup page

--- routes/signup.js
+++ routes/signup.js
@@ -14,13 +14,36 @@ const User = mongoose.models.User || mongoose.model("User", userSchema);
 
 
 router.get("/", (request, response) => {
-    response.render("signup");
+    response.render("signup", {message: ""});
 });
 
 router.post("/submit", async (request, response) => {
-    await addUser(request);
-    response.redirect("/");
+    const found = await checkEmail(request.body.email);
+    if (found) {
+        const message = `<p class="signup-error">Email Already Found. Try Logging In.</p>`;
+        response.render("signup", {message: message});
+    } else {
+        await addUser(request);
+        response.redirect("/");
+    }
 });
+
+async function checkEmail(emailInput) {
+    try {
+        await mongoose.connect(process.env.MONGO_CONNECTION_STRING, { dbName: "contentDB"});
+            const collection = mongoose.connection.db.collection("users");
+            const user = await collection.findOne({email: emailInput});
+            console.log(`\nEmail Found: ${user}`);
+            if (user) {
+                return true;
+            } else {
+                return false;
+            }
+    } catch (err) {
+        console.error(err);
+        return false;
+    }
+}
 
 async function addUser(request) {
     try {
